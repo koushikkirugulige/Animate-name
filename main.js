026f069e612a0b533b182c9a850673113b7ab23e
koushikkirugulige <koushikkirugulige@gmail.com>
2017-08-20 13:17:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var red = [255,0, 0];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+
+var myName = "kirugulige";
+var my= "hello world";
+var newcolor= [orange,blue,red]
+var letterColors = [purple,red,blue]
+if (1>10){
+    bubbleShape="square";
+}
+else {
+    bubbleShape = "circle"
+}
+
+//drawName(me, letterColors);
+drawName(myName,newcolor);
+bounceBubbles();
